e200a32404a6365b56b4723e5c696ac80d8d7658
MishaSabre <mikhailsablin229@gmail.com>
2023-07-01 15:20:12 +0800

    final commit, add func insert, delete, update from postgres

--- bdsite/main/views.py
+++ bdsite/main/views.py
@@ -13,18 +13,22 @@ def doctors(request):
 def add_doctor(request):
     error = ''
     if request.method == 'POST':
-        form = DoctorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('doctors')
-        else:
-            error = 'Неверная форма'
-
+        fio = request.POST.get('fio')
+        specs = request.POST.get('specs')
+        payment = request.POST.get('payment')
+        percentprofit = request.POST.get('percentporfit')
+        diddoctor = -1
+        with connection.cursor() as cursor:
+            cursor.execute('call "INSERT_DOCTOR"(%s,%s,%s,%s,%s)', (fio, specs, payment, percentprofit, diddoctor))
+            cursor.close()
+        return redirect('doctors')
+    else:
+        error = 'Неверная форма'
 
     form = DoctorForm()
     data = {
-        'form' : form,
-        'error' : error
+        'form': form,
+        'error': error
     }
     return render(request, 'main/add_doctor.html', data)
 
@@ -32,16 +36,37 @@ class DoctorDetailView(DetailView):
     model = Doctor
     template_name = 'main/doctor_view.html'
     context_object_name = 'doctor'
-class DoctorUpdateView(UpdateView):
-    model = Doctor
-    template_name = 'main/add_doctor.html'
+def update_doctor(request, pk):
+    error = ''
+    if request.method == 'POST':
+        fio = request.POST.get('fio')
+        specs = request.POST.get('specs')
+        payment = request.POST.get('payment')
+        percentprofit = request.POST.get('percentporfit')
+        diddoctor=pk
+        with connection.cursor() as cursor:
+            cursor.execute('call "UPDATE_DOCTOR"(%s,%s,%s,%s,%s)', (fio, specs, payment, percentprofit, diddoctor))
+            cursor.close()
+        return redirect('doctors')
+    else:
+        error = 'Неверная форма'
+    form = DoctorForm()
 
-    form_class = DoctorForm
+    data = {
+        'form': form,
+        'error': error
+    }
 
-class DoctorDeleteView(DeleteView):
-    model = Doctor
-    success_url = '/'
-    template_name = 'main/delete_doctor.html'
+    return render(request, 'main/add_doctor.html', data)
+
+def doctor_delete(request, pk):
+    if request.method == 'POST':
+        with connection.cursor() as cursor:
+            cursor.execute('call "DELETE_DOCTOR"(%s)', [pk])
+            cursor.close()
+        return redirect('doctors')
+
+    return render(request, 'main/delete_doctor.html')
 
 def patients(request):
     get_patient = Patient.objects.all()
@@ -50,12 +75,18 @@ def patients(request):
 def add_patient(request):
     error = ''
     if request.method == 'POST':
-        form = PatientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('patients')
-        else:
-            error = 'Неверная форма'
+        surname = request.POST.get('surname')
+        name = request.POST.get('name')
+        patronymic = request.POST.get('patronymic')
+        bdate = request.POST.get('bdate')
+        address = request.POST.get('address')
+        didpatient= -1
+        with connection.cursor() as cursor:
+            cursor.execute('call "INSERT_PATIENT"(%s,%s,%s,%s,%s', (surname, name, patronymic, bdate, address, didpatient))
+            cursor.close()
+        return redirect('patients')
+    else:
+        error = 'Неверная форма'
 
     form = PatientForm()
     data = {
@@ -67,16 +98,35 @@ class PatientDetailView(DetailView):
     model = Patient
     template_name = 'main/patient_view.html'
     context_object_name = 'patient'
-class PatientUpdateView(UpdateView):
-    model = Patient
-    template_name = 'main/add_patient.html'
+def update_patient(request, pk):
+    error = ''
+    if request.method == 'POST':
+        surname = request.POST.get('surname')
+        name = request.POST.get('name')
+        patronymic = request.POST.get('patronymic')
+        bdate = request.POST.get('bdate')
+        address = request.POST.get('address')
+        with connection.cursor() as cursor:
+            cursor.execute('call "UPDATE_PATIENT"(%s,%s,%s,%s,%s, %s)', (surname, name, patronymic, bdate, address, pk))
+            cursor.close()
+        return redirect('patients')
+    else:
+        error = 'Неверная форма'
 
-    form_class = PatientForm
+    form = PatientForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/add_patient.html', data)
+def patient_delete(request, pk):
+    if request.method == 'POST':
+        with connection.cursor() as cursor:
+            cursor.execute('call "DELETE_PATIENT"(%s)', [pk])
+            cursor.close()
+        return redirect('patients')
 
-class PatientDeleteView(DeleteView):
-    model = Doctor
-    success_url = '/patients'
-    template_name = 'main/delete_patient.html'
+    return render(request, 'main/delete_patient.html')
 
 def visits(request):
     get_visit = Visits.objects.all()
@@ -85,12 +135,17 @@ def visits(request):
 def add_visit(request):
     error = ''
     if request.method == 'POST':
-        form = VisitsForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('visits')
-        else:
-            error = 'Неверная форма'
+        iddoctor = request.POST.get('iddoctor')
+        idpatient = request.POST.get('idpatient')
+        date_of_visit = request.POST.get('date_of_visit')
+        idvisits = -1
+        with connection.cursor() as cursor:
+            cursor.execute('call "INSERT_VISITS"(%s,%s,%s,%s)',
+                           (iddoctor, idpatient, date_of_visit, idvisits))
+            cursor.close()
+        return redirect('visits')
+    else:
+        error = 'Неверная форма'
 
     form = VisitsForm()
     data = {
@@ -102,16 +157,36 @@ class VisitDetailView(DetailView):
     model = Visits
     template_name = 'main/visits_view.html'
     context_object_name = 'visit'
-class VisitUpdateView(UpdateView):
-    model = Visits
-    template_name = 'main/add_visit.html'
+def update_visit(request, pk):
+    error = ''
+    if request.method == 'POST':
+        iddoctor = request.POST.get('iddoctor')
+        idpatient = request.POST.get('idpatient')
+        date_of_visit = request.POST.get('date_of_visit')
+        idvisits = pk
+        with connection.cursor() as cursor:
+            cursor.execute('call "UPDATE_VISITS"(%s,%s,%s,%s)',
+                           (iddoctor, idpatient, date_of_visit, idvisits))
+            cursor.close()
+        return redirect('visits')
+    else:
+        error = 'Неверная форма'
 
-    form_class = VisitsForm
+    form = VisitsForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/add_visit.html', data)
 
-class VisitDeleteView(DeleteView):
-    model = Visits
-    success_url = '/visits'
-    template_name = 'main/delete_visit.html'
+def visit_delete(request, pk):
+    if request.method == 'POST':
+        with connection.cursor() as cursor:
+            cursor.execute('call "DELETE_VISITS"(%s)', [pk])
+            cursor.close()
+        return redirect('visits')
+
+    return render(request, 'main/delete_visit.html')
 
 def payment_rep(request):
 
